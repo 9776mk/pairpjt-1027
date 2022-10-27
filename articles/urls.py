e75ad3eb559f0944f0934eb4a6537601d187cc93
9776mk <9776mk@gmail.com>
2022-10-27 12:56:15 +0900

    개발환경 설정

---
+++ articles/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from . import views
+from django.conf import settings
+from django.conf.urls.static import static
+
+app_name = "articles"
+
+urlpatterns = [
+    path("", views.index, name="index"),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
