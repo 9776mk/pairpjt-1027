8b86e4a3d0c4438338d1f5d40f9855d89bac66db
dortkthf <dortkthf@gmail.com>
2022-10-28 13:48:21 +0900

    make profile

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,6 +1,9 @@
+from django import forms
 from django.contrib.auth import get_user_model
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.forms import UserChangeForm
+from django import forms
+from accounts.models import Profile
 
 class SignupForm(UserCreationForm):
     class Meta(UserCreationForm.Meta):
@@ -14,4 +17,13 @@ class UpdateForm(UserChangeForm):
             'email' : '이메일',
             'first_name' : '이름',
             'last_name' : '성'
+        }
+
+class ProfileForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields = ['image', 'status_message']
+        labels = {
+            'image' : '프로필 이미지',
+            'status_message' : '상태메시지',
         }
